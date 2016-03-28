599aaa6e3515b9dc56830f157e461f248ce562ba
Tarun <Tarun@192.168.1.28>
2016-03-28 20:11:56 +0200

    Initial start

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+	public static void main(String[] args){
+		System.out.println("Hello all");
+		System.out.println("Creating GIT repository");
+		System.out.println("Share all ur ideas here");
+	}
+
+}
